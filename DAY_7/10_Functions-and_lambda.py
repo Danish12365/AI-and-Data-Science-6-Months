20e9a9297416032fe814536c3294f76940ea8292
Danish12365 <danishazeem365@gmail.com>
2024-02-29 16:28:14 +0500

    functions

---
+++ DAY_7/10_Functions-and_lambda.py
@@ -0,0 +1,123 @@
+# define the function
+# def greeting():
+#     print("Hello, World!")
+# greeting()  # Call the function
+
+
+# def AOA():
+#     print("Aslam o Alikum")
+# AOA()
+
+# def AOA(name):
+#     print(f"Aslam o Alikum {name} How are you?")
+# AOA("Danish")
+
+
+# def AOA(name = "no name"):    # default value is given to the parameter if no argument is passed
+#     print(f"Aslam o Alikum {name} How are you?") 
+# AOA()
+
+
+# Return statement
+
+# def square(number):   # define the function
+#     return number * number
+# print(square(5))       # Output
+
+
+# def square(number):   # define the function
+#     return number * number
+# result = square(5)      # store the output in a variable
+# print(result)           # Print the result
+
+
+# def fectoreal(number):
+#     if number == 0:
+#         return 1
+#     else:
+#         return number / fectoreal(number - 1)
+# print(fectoreal(4))
+
+# lambda function , inline function , a lambda expression or Anonymous Function
+
+# def x(a):
+#     return a + 10
+# print(x(20))
+
+# x = lambda a : a + 10    # define the function
+# print(x(20))
+
+
+# def x(a, b):
+#     return a * b
+# print(x(5,4))
+
+# Anonymous Function (Lambda Function)
+
+# Multiply every element of list
+
+# x = lambda a, b : a * b    # define the function
+# print(x(5,4))
+# result = (lambda x, y: x + y)(10, 20)
+
+
+
+# Assighnment: ap nay 10 function define kerney hain def kay sath be or lambda kay sath be with different opperators.
+
+
+# #Function to add two numbers using def:
+# def add(x, y):
+#     return x + y
+# print(add(10,20))
+
+# # # Function to subtract two numbers using lambda:
+# subtract = lambda x, y: x - y
+# print(subtract(20,10))
+
+# # # Function to multiply two numbers using def:
+# def multiply(x, y):
+#     return x * y
+# print(multiply(10,5))
+
+# # # Function to divide two numbers using lambda:
+# divide = lambda x, y: x / y
+# print(divide(10,2))
+
+# # # Function to calculate the remainder of division using def:
+# def remainder(x, y):
+#     return x % y
+# print(remainder(10,3))
+
+# # # Function to raise a number to a power using lambda:
+# power = lambda x, y: x ** y
+# print(power(10,2))
+
+# # # Function to calculate the floor division using def:
+# def floor_divide(x, y):
+#     return x // y
+# print(floor_divide(10,2))
+
+# # # Function to find the maximum of two numbers using lambda:
+# maximum = lambda x, y: x if x > y else y
+# print(maximum(10,20))
+
+# # # Function to find the minimum of two numbers using def:
+# def minimum(x, y):
+#     return x if x < y else y
+# print(minimum(10,20))
+
+# # # Function to calculate the absolute value of a number using lambda:
+# absolute = lambda x: abs(x)
+# print(absolute(-10))
+
+
+
+
+
+
+
+
+
+
+
+
