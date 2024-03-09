d9e1bbdb04dc5240652df4ebe30a56c2d27e9218
Danish12365 <danishazeem365@gmail.com>
2024-03-09 15:57:43 +0500

    Changed the function call from `greeting()` to `# greeting()`.

---
+++ DAY_7/12_errors.py
@@ -0,0 +1,68 @@
+# syntax or indentation error
+# x = 5
+# if x > 0:
+#   print("x is positive")
+
+
+# logical or human error
+if x > 3:
+    print("y is positive")
+
+# runtime or module not found
+import numpy as np
+print(np.sqrt(9))   
+    
+
+
+
+# 1. SyntaxError
+# 2. ZeroDivisionError
+# 3. NameError
+# 4. TypeError
+# 5. ValueError
+# 6. IndexError
+# 7. KeyError
+# 8. FileNotFoundError
+# 9. ImportError
+# 10. AttributeError
+# 11. KeyboardInterrupt
+# 12. MemoryError
+# 13. OverflowError
+# 14. RecursionError
+# 15. StopIteration
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
